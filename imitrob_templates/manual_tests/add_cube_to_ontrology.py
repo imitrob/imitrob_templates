70c1ca23930bb1a646e3584d80ca44fe2e707acd
Petr Vanc <petr.vanc@cvut.cz>
2024-01-25 17:47:46 +0100

    update

--- imitrob_templates/manual_tests/add_cube_to_ontrology.py
+++ imitrob_templates/manual_tests/add_cube_to_ontrology.py
@@ -1,5 +1,4 @@
-
-import rclpy, time
+import rclpy, time, sys
 from rclpy.node import Node
 from teleop_msgs.msg import HRICommand
 from crow_msgs.msg import FilteredPose, PclDimensions
@@ -12,44 +11,61 @@ def main():
     rosnode = Node("add_dummy_cube__node")
     soc = SceneOntologyClient(rosnode)
     
-    # pub = rosnode.create_publisher(HRICommand, "/hri/command", 5)
+    pub = rosnode.create_publisher(HRICommand, "/hri/command", 5)
     
-    # soc.add_dummy_cube()
+    soc.add_dummy_cube()
+    
+    print(soc.get_objects_from_onto())
 
-    '''
-    # from std_msgs.msg import Float32MultiArray
+def scene_experimental_setup_node(objects={
+    'wheel': {
+        'pose': Pose(position=Point(x=0.2,y=0.0,z=0.1),orientation=Quaternion(x=1.0,y=0.0,z=0.0,w=0.0)),
+        'uuid': '586da263-0d69-000d-8a68-f51a511c4e66', 
+        'size': PclDimensions(dimensions=[0.1,0.1,0.1]),
+        'tracked': True,
+    },
+    'cube_holes': {
+        'pose': Pose(position=Point(x=0.1,y=0.0,z=0.1),orientation=Quaternion(x=1.0,y=0.0,z=0.0,w=0.0)),
+        'uuid': '586da263-0d69-001d-8a68-f51a511c4e66', 
+        'size': PclDimensions(dimensions=[0.1,0.1,0.1]),
+        'tracked': True,            
+    },
+    }):
+    """ object_adder_node
+    Constantly publishes predefined set of objects to the ontology to fulfill test
+
+    Args:
+        objects (dict, optional): _description_. 
+    """    
+    
+    poses = []
+    sizes = []
+    uuids = []
+    tracked = []
+    labels = []
+    for obj_key in objects.keys():
+        obj = objects[obj_key]
+        poses.append(obj['pose'])
+        sizes.append(obj['size'])
+        uuids.append(obj['uuid'])
+        tracked.append(obj['tracked'])
+        labels.append(obj_key)
+
+    pubmsg = FilteredPose(poses=poses,size=sizes,tracked=tracked,label=labels,uuid =uuids)
+    
+    rclpy.init()
+    rosnode = Node("adder_tester")
     pub = rosnode.create_publisher(FilteredPose, "/filtered_poses", 5)
-    pose = Pose(position=Point(x=0.0,y=0.0,z=0.1),orientation=Quaternion(x=0.0,y=0.0,z=0.0,w=0.0))
-    pcldim = PclDimensions(dimensions=[0.1,0.1,0.1])
-    label = 'wheel'
-    uuid = '586da263-0d69-447d-8a68-f51a511c4e66'
-    # particle = Float32MultiArray() #std_msgs/Float32MultiArray[] 
-
-    pubmsg = FilteredPose( 
-    # This msg wraps pose of object + its label
-    poses = [pose],
-    size = [pcldim],
-    tracked = [True],
-    label = [label], #must be one of the class labels from detector
-    uuid = [uuid], #identifying model in filter
-    #confidence = 1.0, #confidence, can be empty
-    #crow_msgs/Particles[] particles #model_particles, can be empty
-    # particles = [particle]
-    )
-
+    
     while True:
-            t = time.time()
-            print(t)
-            pubmsg.header.stamp.sec = int(t)
-            pubmsg.header.stamp.nanosec = int(t % 1 * (10 ** 9))
+            pubmsg.header.stamp = rosnode.get_clock().now().to_msg()
             pub.publish(pubmsg)
             time.sleep(0.5)
 
     rclpy.spin(rosnode)
 
-    '''
-    
-    print(soc.get_objects_from_onto())
-    
 if __name__ == '__main__':
-    main()
+    if len(sys.argv[1]) > 1:
+        eval(sys.argv[1]+'()')
+    else:
+        main()
